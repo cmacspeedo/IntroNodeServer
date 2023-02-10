70fa708b25334b8b7a34034ad3960df9bfe3d9e7
cmacspeedo <cmacspeedo101@gmail.com>
2023-02-10 10:03:38 -0700

    added folders and 2 other routes

--- controllers/index.js
+++ controllers/index.js
@@ -10,4 +10,16 @@ const ttech =
     res.send("<h1>Tooele Tech is Awesome!</h1>");
   });
 
-module.exports = { awesomeFunction, ttech };
+const school =
+  ("/school",
+  (req, res) => {
+    res.send("<h1>Isn't school just great!?</h1>");
+  });
+
+const work =
+  ("/work",
+  (req, res) => {
+    res.send("<h1>Work is not so great...</h1>");
+  });
+
+module.exports = { awesomeFunction, ttech, school, work };
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,7 @@ const routes = require("express").Router();
 
 routes.get("/", myController.awesomeFunction);
 routes.get("/ttech", myController.ttech);
+routes.get("/school", myController.school);
+routes.get("/work", myController.work);
 
 module.exports = routes;
