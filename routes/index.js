a7c7e1cdabc234bbec48c9dcfa3e1c1b8bc41e7e
cmacspeedo <cmacspeedo101@gmail.com>
2023-02-10 13:17:00 -0700

    undo commented out pieces

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const routes = require("express").Router();
 
 routes.get("/", myController.awesomeFunction);
 routes.get("/ttech", myController.ttech);
-// routes.get("/school", myController.school);
-// routes.get("/work", myController.work);
+routes.get("/school", myController.school);
+routes.get("/work", myController.work);
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -2,10 +2,6 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
-
 app.listen(PORT, console.log(`Test server running on port: ${PORT}`));
 
 app.use("/", require("./routes"));
