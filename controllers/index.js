ab6dbcb865fc4ba42c048691f62e0627d4cbc958
cmacspeedo <cmacspeedo101@gmail.com>
2023-02-10 12:35:12 -0700

    retry

--- controllers/index.js
+++ controllers/index.js
@@ -10,18 +10,18 @@ const ttech =
     res.send("<h1>Tooele Tech is Awesome!</h1>");
   });
 
-const school =
-  ("/school",
-  (req, res) => {
-    res.send("<h1>Isn't school just great!?</h1>");
-  });
+// const school =
+//   ("/school",
+//   (req, res) => {
+//     res.send("<h1>Isn't school just great!?</h1>");
+//   });
 
-const work =
-  ("/work",
-  (req, res) => {
-    res.send("<h1>Work is not so great...</h1>");
-  });
+// const work =
+//   ("/work",
+//   (req, res) => {
+//     res.send("<h1>Work is not so great...</h1>");
+//   });
 
-module.exports = { awesomeFunction, ttech, school, work };
+module.exports = { awesomeFunction, ttech };
 
 // File("/index.html", { root: __dirname }
