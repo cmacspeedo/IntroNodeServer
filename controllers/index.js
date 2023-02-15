4f8eac919c5cba24bcc22157fcf617b8d973c1d0
cmacspeedo <cmacspeedo101@gmail.com>
2023-02-14 17:55:47 -0700

    part3

--- controllers/index.js
+++ controllers/index.js
@@ -21,7 +21,120 @@ const getAllStudents = async (req, res) => {
   }
 };
 
-module.exports = { awesomeFunction, tooeleTechFunction, getAllStudents };
+// Get single contact
+const getSingleStudent = async (req, res) => {
+  try {
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb
+      .getDb()
+      .db()
+      .collection("Students")
+      .find({ _id: userId });
+    result.toArray().then((lists) => {
+      res.setHeader("Content-type", "application/json");
+      res.status(200).json(lists[0]);
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+// CREATE contact
+const createStudent = async (req, res) => {
+  try {
+    const student = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      age: req.body.age,
+      currentCollege: req.body.currentCollege,
+    };
+
+    const response = await mongodb
+      .getDb()
+      .db()
+      .collection("students")
+      .instertOne(student);
+    if (response.acknowledged) {
+      res.status(201).json(response);
+    } else {
+      res
+        .status(500)
+        .json(
+          response.error || "Some error occurred while creating the student"
+        );
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+// UPDATE one student
+const updateStudent = async (req, res) => {
+  try {
+    const userId = new ObjectId(req.params.id);
+    // be aware of updateOne if you only want to update specific fields
+    const student = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      age: req.body.age,
+      currentCollege: req.body.currentCollege,
+    };
+
+    const response = await mongodb
+      .getDb()
+      .db()
+      .collection("Students")
+      .replaceOne({ _id: userId }, student);
+    console.log(response);
+    if (response.modifiedCount > 0) {
+      res.status(204).send(response);
+    } else {
+      res
+        .status(500)
+        .json(
+          response.error || "Some error occured while updating the student."
+        );
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+// DELETE one student
+const deleteStudent = async (req, res) => {
+  try {
+    const userId = new ObjectId(req.params.id);
+    const response = await mongodb
+      .getDb()
+      .db()
+      .collection("Students")
+      .deleteOne({ _id: userId }, true);
+    console.log(response);
+    if (response.deletedCount > 0) {
+      res.status(200).send();
+    } else {
+      res
+        .statu(500)
+        .json(
+          response.error || "Some error occured while deleting the student."
+        );
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+module.exports = {
+  awesomeFunction,
+  tooeleTechFunction,
+  getAllStudents,
+  getSingleStudent,
+  createStudent,
+  updateStudent,
+  deleteStudent,
+};
 
 // const awesomeFunction =
 //   ("/",
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const myController = require("../controllers");
 const routes = require("express").Router();
 
 routes.get("/", myController.awesomeFunction);
-routes.get("/ttech", myController.tooeleTechFunction);
-routes.get("/students", myController.getAllStudents);
-// routes.get("/work", myController.work);
+routes.get("/ttech", myController.getAllStudents);
+// student routes
+// routes.get("/", myController.updateStudent);
+
+routes.use("/students", require("./students"));
 
 module.exports = routes;
